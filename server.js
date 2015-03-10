3746ca8c8d6bf7ab50776834230cf810793ced22
ryanblanks <ryan.blanks@gmail.com>
2015-03-10 14:02:29 -0500

    Remove unnecessary references
    Don’t need session, cookieParser, or connect-flash.  Remove references
    and use statements.

--- server.js
+++ server.js
@@ -2,20 +2,8 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var controllers = require("./controllers");
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
 
 app.set("view engine", "vash");
-
-//Opt into Services
-app.use(session({ 
-	secret: 'myNewTimeTrackerSight',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(cookieParser());
-app.use(flash());
 
 //set the public static resource folder
 app.use(express.static(__dirname + "/public"));
